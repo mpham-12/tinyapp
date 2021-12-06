2cd5bde44a2bc7bddff555502da745a0e9a9b9ed
Martin Pham <martinp0512@gmail.com>
2021-12-06 18:55:43 +0000

    comments added for understanding

--- express_server.js
+++ express_server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express'); 
 const app = express();
 const PORT = 8080;
 
@@ -7,6 +7,8 @@ const urlDatabase ={
   "9sm5xK": "http://www.google.com"
 };
 
+// express.get takes in 2 parameters (request, response). 
+// When client is connected, they recieve "hello" on their end.
 app.get('/', (req, res)=> {
   res.send('hello!');
 });
@@ -15,10 +17,12 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// Incorporates JS and HTML. Responds with bolded text. 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// Allows server to retrieve or "listen" to requests.
 app.listen(PORT, ()=> {
   console.log(`Example app listening on ${PORT}!`)
-})
+});
